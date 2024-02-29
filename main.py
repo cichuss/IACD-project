b43b301767d32c653aa3433f1880c24b3caa80c6
Magda <266857@student.pwr.edu.pl>
2024-02-29 01:16:09 +0000

    somehow works

--- main.py
+++ main.py
@@ -13,22 +13,30 @@ def evaluate_basic(formula, prop_dict):
     """
     Evaluates the formula without considering parentheses.
     """
+    skip_next = False
     stack = []
-    for char in formula:
-        if char.isalpha():
+    for index, char in enumerate(formula):
+        if skip_next:
+            skip_next = False
+        elif char == '!':
+            stack.append(not prop_dict[formula[index + 1]])
+            skip_next = True
+        elif char.isalpha():
             stack.append(prop_dict[char])
-        elif char == '-':
-            stack.append(not stack.pop())
         elif char == '&':
-            stack.append(stack.pop() and stack.pop())
+            stack.append(stack.pop() and prop_dict[formula[index + 1]])
+            skip_next = True
         elif char == '|':
-            stack.append(stack.pop() or stack.pop())
+            stack.append(stack.pop() or prop_dict[formula[index + 1]])
+            skip_next = True
         elif char == '>':
-            a, b = stack.pop(), stack.pop()
+            a, b = stack.pop(), prop_dict[formula[index + 1]]
             stack.append(not a or b)
+            skip_next = True
         elif char == '=':
-            a, b = stack.pop(), stack.pop()
+            a, b = stack.pop(), prop_dict[formula[index + 1]]
             stack.append(a == b)
+            skip_next = True
     return stack[0]
 
 
@@ -37,15 +45,18 @@ def evaluate(formula, prop_dict):
     Evaluates the formula considering parentheses.
     """
     stack = []
+    prop_dict['T'] = True
+    prop_dict['F'] = False
     for char in formula:
-        if char == '(':
-            stack.append(char)
-        elif char == ')':
+        if char == ')':
             sub_formula = ''
             while stack[-1] != '(':
-                sub_formula = stack.pop() + sub_formula
+                sub_formula = str(stack.pop()) + sub_formula
             stack.pop()  # Pop '('
-            stack.append(str(evaluate_basic(sub_formula, prop_dict)))
+            if evaluate_basic(sub_formula, prop_dict):
+                stack.append('T')
+            else:
+                stack.append('F')
         else:
             stack.append(char)
     return evaluate_basic(''.join(stack), prop_dict)
@@ -79,15 +90,42 @@ def determine_type(table, prop_dict):
     return "Contingency"
 
 
+def add_parentheses(formula):
+    formula2 = ""
+    skip_next = False
+    skip_next2 = False
+    for i, char in enumerate(formula):
+        if skip_next:
+            skip_next = False
+        elif skip_next2:
+            skip_next2 = False
+        elif char == "!":
+            formula2 = formula2 + "(" + char + formula[i + 1] + ")"
+            skip_next = True
+        elif ((char == "|") or (char == "&")) and (formula2[-1] != ")") and (formula[i+1] != "!"):
+            formula2 = formula2[:-1]
+            formula2 = formula2 + "(" + formula[i - 1] + char + formula[i + 1] + ")"
+            skip_next = True
+        elif ((char == "|") or (char == "&")) and (formula2[-1] != ")"):
+            formula2 = formula2[:-1]
+            formula2 = formula2 + "(" + formula[i - 1] + char + "(" + formula[i + 1] + formula[i + 2] + "))"
+            skip_next = True
+            skip_next2 = True
+        else:
+            formula2 += char
+    return formula2
+
+
 def main():
     formula = input("Enter a well-formed formula in propositional logic: ").replace(" ", "")
-
     prop_dict = {}
+    formula = add_parentheses(formula)
+    print(formula)
     for char in formula:
-        if not is_atomic(char, prop_dict) and char not in ['-', '&', '|', '>', '=', '(', ')']:
+        if not is_atomic(char, prop_dict) and char not in ['!', '&', '|', '>', '=', '(', ')']:
             print("ERROR: Invalid character detected.")
             return
-
+    print(prop_dict)
     truth_table = generate_truth_table(prop_dict)
 
     print("\nTruth Table:")
@@ -107,4 +145,5 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    while True:
+        main()
